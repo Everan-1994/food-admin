74f80bd1274265ca335f87b0d996ec9f18fb241a
everan-1994 <everan@aliyun.com>
2019-05-28 11:25:13 +0800

    update news api

--- app/Http/Resources/NewsResource.php
+++ app/Http/Resources/NewsResource.php
@@ -9,19 +9,20 @@ class NewsResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'type' => $this->type,
-            'title' => $this->title,
-            'image' => $this->image,
-            'intro' => $this->intro,
-            'from' => $this->from,
-            'created_at' => $this->created_at->toDateString()
+            'id'         => $this->id,
+            'type'       => $this->type,
+            'title'      => $this->title,
+            'image'      => $this->image,
+            'intro'      => $this->intro,
+            'content'    => $this->content,
+            'from'       => $this->from,
+            'created_at' => $this->created_at->toDateString(),
         ];
     }
 }
